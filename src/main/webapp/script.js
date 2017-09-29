78f7d838627f33026c02117c085ce06e695f1930
snowwhiskers <tiiupiiber@gmail.com>
2017-09-29 19:52:49 +0300

    memory: bed.jpg, big,jpg, kontroll ja automaatne refresh

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -23,26 +23,50 @@ function changeColor(newColor) {
           elem.style.color = newColor;
 }
 
-function changeImage(i) {
+var score = 0;
+var currentImage = "";
+var previousImage = "";
+var counter = 0;
 
+function changeImage(i) {
       switch (i) {
           case 'A':
+            currentImage = "media/steps.JPG";
             document.getElementById("steps").src = "media/steps.JPG";
+            console.log("checkIfSame " + currentImage + previousImage);
+            checkIfSame();
+
+            previousImage = currentImage;
             break;
           case 'B':
+            currentImage = "media/bed.jpg";
             document.getElementById("bed").src = "media/bed.jpg";
+            checkIfSame();
+            previousImage = currentImage;
             break;
           case 'C':
+            currentImage = "media/big.jpg";
             document.getElementById("big").src = "media/big.jpg";
+            checkIfSame();
+            previousImage = currentImage;
             break;
           case 'D':
+            currentImage = "media/big.jpg";
             document.getElementById("big2").src = "media/big.jpg";
+            checkIfSame();
+            previousImage = currentImage;
             break;
           case 'E':
+            currentImage = "media/steps.JPG";
             document.getElementById("steps2").src = "media/steps.JPG";
+            checkIfSame();
+            previousImage = currentImage;
             break;
           case 'F':
+            currentImage = "media/bed.jpg";
             document.getElementById("bed2").src = "media/bed.jpg";
+            checkIfSame();
+            previousImage = currentImage;
             break;
           default:
             console.log('Invalid item');
@@ -60,9 +84,38 @@ function changeImage(i) {
 }
 
 function refresh() {
+    //all pictures
     var pictures = document.querySelectorAll(".refresh");
+    score = 0;
+    var currentImage = "";
+    var previousImage = "";
+    counter = 0;
     //console.log(pictures);
-    for (var i = 0; i < pictures.length; i++) {
-        pictures.item(i).src = "media/blossoms.JPG";
+    return function() {
+        for (var i = 0; i < pictures.length; i++) {
+                pictures.item(i).src = "media/blossoms.JPG";
+        }
     }
+}
+
+//document.getElementById("demo").addEventListener("click", checkIfSame());
+
+
+function checkIfSame() {
+    if (counter == 1) {
+
+        if (currentImage == previousImage) {
+            score = score++;
+            counter = 0;
+            console.log("same!");
+        } else {
+            setTimeout(refresh(), 1000);
+
+            console.log("not same!");
+        }
+    } else {
+        counter++;
+        console.log("counter++");
+    }
+
 }
